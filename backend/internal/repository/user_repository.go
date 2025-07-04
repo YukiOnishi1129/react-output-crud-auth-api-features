8d7b91e06c11edafa068352bfdf5d4fa560bc022
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-07-04 22:22:31 +0900

    init

---
+++ backend/internal/repository/user_repository.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/YukiOnishi1129/react-output-crud-auth-api/backend/internal/infrastructure/persistence/dto"
+)
+
+
+type UserRepository interface {
+	FindByEmail(ctx context.Context, input *dto.FindUserByEmailInput) (*dto.UserOutput, error)
+	Create(ctx context.Context, input *dto.CreateUserInput) (*dto.UserOutput, error)
+}
